3ecb7467271b32fb42bd4bff6d9dc7955e45e572
James Tunnell <jamestunnell@gmail.com>
2023-03-21 14:15:13 -0700

    Print profie/loss probabilities

--- cmd/sim-returns/main.go
+++ cmd/sim-returns/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"os"
 	"time"
@@ -67,37 +68,87 @@ func main() {
 		processReturn = mulRatio
 	}
 
-	endBalances := make([]float64, params.Runs)
+	results := make([]float64, params.Runs)
 
 	for i := 0; i < params.Runs; i++ {
-		endBalances[i] = simReturns(params, processReturn)
+		results[i] = simReturns(params, processReturn)
 	}
 
-	logStats(endBalances)
-	printHist(endBalances, params.Bins)
+	printStats(results)
+	printProbs(results, params.Start)
+	printHist(results, params.Bins)
 }
 
-func logStats(x []float64) {
+func printStats(x []float64) {
 	min, _ := stats.Min(x)
 	max, _ := stats.Max(x)
 	median, _ := stats.Median(x)
 	mean, _ := stats.Mean(x)
+	stddev, _ := stats.StandardDeviation(x)
 
-	log.Info().
-		Float64("min", min).
-		Float64("max", max).
-		Float64("median", median).
-		Float64("mean", mean).
-		Msg("stats")
+	fmt.Println("Stats:")
+	fmt.Printf("  Min: %f\n", min)
+	fmt.Printf("  Max: %f\n", max)
+	fmt.Printf("  Median: %f\n", median)
+	fmt.Printf("  Mean: %f\n", mean)
+	fmt.Printf("  Std Dev: %f\n", stddev)
+	fmt.Println("")
+}
+
+func printProbs(vals []float64, start float64) {
+	fmt.Println("Loss Probabilities:")
+	fmt.Printf("  25%% or more: %f%%\n", 100.0*probLE(vals, start*0.75))
+	fmt.Printf("  50%% or more: %f%%\n", 100.0*probLE(vals, start*0.5))
+	fmt.Printf("  75%% or more: %f%%\n", 100.0*probLE(vals, start*0.25))
+	fmt.Printf("  100%% or more: %f%%\n", 100.0*probLE(vals, 0))
+	fmt.Println("")
+
+	fmt.Println("Profit Probabilities:")
+	fmt.Printf("  25%% or more: %f%%\n", 100.0*probGE(vals, start*1.25))
+	fmt.Printf("  50%% or more: %f%%\n", 100.0*probGE(vals, start*1.5))
+	fmt.Printf("  75%% or more: %f%%\n", 100.0*probGE(vals, start*1.75))
+	fmt.Printf("  100%% or more: %f%%\n", 100.0*probGE(vals, start*2))
+	fmt.Println("")
+}
+
+func probLE(vals []float64, level float64) float64 {
+	le := func(val float64) bool {
+		return val <= level
+	}
+
+	return float64(count(vals, le)) / float64(len(vals))
+}
+
+func probGE(vals []float64, level float64) float64 {
+	ge := func(val float64) bool {
+		return val >= level
+	}
+
+	return float64(count(vals, ge)) / float64(len(vals))
+}
+
+func count[T any](ts []T, f func(t T) bool) int {
+	count := 0
+
+	for _, t := range ts {
+		if f(t) {
+			count += 1
+		}
+	}
+
+	return count
 }
 
 func printHist(x []float64, bins int) {
+	fmt.Println("Histogram:")
 	hist := histogram.Hist(bins, x)
 
 	err := histogram.Fprint(os.Stdout, hist, histogram.Linear(5))
 	if err != nil {
 		log.Warn().Err(err).Msg("failed to print histogram")
 	}
+
+	fmt.Println("")
 }
 
 func getParams() *Params {
@@ -122,13 +173,13 @@ func getParams() *Params {
 		Bins:  *bins,
 	}
 
-	log.Info().
-		Int("runs", params.Runs).
-		Int("iter", params.Iter).
-		Int("bins", params.Bins).
-		Float64("min", params.Min).
-		Float64("max", params.Max).
-		Msg("params")
+	fmt.Println("Params:")
+	fmt.Printf("  Runs: %d\n", params.Runs)
+	fmt.Printf("  Iter: %d\n", params.Iter)
+	fmt.Printf("  Bins: %d\n", params.Bins)
+	fmt.Printf("  Min: %f\n", params.Min)
+	fmt.Printf("  Max: %f\n", params.Max)
+	fmt.Println("")
 
 	return params
 }
