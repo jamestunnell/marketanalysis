f3a634844d4e8ea2164ae1b9c3998479cd6caa9f
jamestunnell <jamestunnell@gmail.com>
2024-04-09 20:17:42 +0000

    Add HeikinAshi block

--- blocks/blocks.go
+++ blocks/blocks.go
@@ -3,10 +3,11 @@ package blocks
 func init() {
 	reg.Add(TypeAroon, NewAroon)
 	reg.Add(TypeATR, NewATR)
-	reg.Add(TypeBarValue, NewBarValue)
+	reg.Add(TypeBar, NewBar)
 	reg.Add(TypeDMI, NewDMI)
 	reg.Add(TypeEMA, NewEMA)
 	reg.Add(TypeEMV, NewEMV)
+	reg.Add(TypeHeikinAshi, NewHeikinAshi)
 	reg.Add(TypeMAOrder, NewMAOrder)
 	reg.Add(TypeSMA, NewSMA)
 	reg.Add(TypeSupertrend, NewSupertrend)
--- blocks/heikinashi.go
+++ blocks/heikinashi.go
@@ -3,11 +3,12 @@ package blocks
 import "github.com/jamestunnell/marketanalysis/models"
 
 const (
-	DescrBarValue = "Value from bar data"
-	TypeBarValue  = "BarValue"
+	DescrHeikinAshi = "Heikin-Ashi bar data values"
+	TypeHeikinAshi  = "HeikinAshi"
 )
 
-type BarValue struct {
+type HeikinAshi struct {
+	prev  *models.OHLC
 	open  *models.TypedOutput[float64]
 	high  *models.TypedOutput[float64]
 	low   *models.TypedOutput[float64]
@@ -19,8 +20,9 @@ type BarValue struct {
 	hlcc4 *models.TypedOutput[float64]
 }
 
-func NewBarValue() models.Block {
-	return &BarValue{
+func NewHeikinAshi() models.Block {
+	return &HeikinAshi{
+		prev:  nil,
 		open:  models.NewTypedOutput[float64](),
 		high:  models.NewTypedOutput[float64](),
 		low:   models.NewTypedOutput[float64](),
@@ -33,23 +35,23 @@ func NewBarValue() models.Block {
 	}
 }
 
-func (blk *BarValue) GetType() string {
-	return TypeBarValue
+func (blk *HeikinAshi) GetType() string {
+	return TypeHeikinAshi
 }
 
-func (blk *BarValue) GetDescription() string {
-	return DescrBarValue
+func (blk *HeikinAshi) GetDescription() string {
+	return DescrHeikinAshi
 }
 
-func (blk *BarValue) GetParams() models.Params {
+func (blk *HeikinAshi) GetParams() models.Params {
 	return models.Params{}
 }
 
-func (blk *BarValue) GetInputs() models.Inputs {
+func (blk *HeikinAshi) GetInputs() models.Inputs {
 	return models.Inputs{}
 }
 
-func (blk *BarValue) GetOutputs() models.Outputs {
+func (blk *HeikinAshi) GetOutputs() models.Outputs {
 	return models.Outputs{
 		NameOpen:  blk.open,
 		NameHigh:  blk.high,
@@ -63,23 +65,35 @@ func (blk *BarValue) GetOutputs() models.Outputs {
 	}
 }
 
-func (blk *BarValue) IsWarm() bool {
-	return true
+func (blk *HeikinAshi) IsWarm() bool {
+	return blk.prev != nil
 }
 
-func (blk *BarValue) Init() error {
+func (blk *HeikinAshi) Init() error {
 	return nil
 }
 
-func (blk *BarValue) Update(cur *models.Bar) {
-	blk.open.Set(cur.Open)
-	blk.high.Set(cur.High)
-	blk.low.Set(cur.Low)
-	blk.close.Set(cur.Close)
+func (blk *HeikinAshi) Update(cur *models.Bar) {
+	defer blk.updatePrev(cur)
 
-	blk.hl2.SetIfConnected(cur.HL2)
-	blk.hlc3.SetIfConnected(cur.HLC3)
-	blk.occ3.SetIfConnected(cur.OCC3)
-	blk.ohlc4.SetIfConnected(cur.OHLC4)
-	blk.hlcc4.SetIfConnected(cur.HLCC4)
+	if blk.prev == nil {
+		return
+	}
+
+	ha := cur.HeikinAshi(blk.prev)
+
+	blk.open.Set(ha.Open)
+	blk.high.Set(ha.High)
+	blk.low.Set(ha.Low)
+	blk.close.Set(ha.Close)
+
+	blk.hl2.SetIfConnected(ha.HL2)
+	blk.hlc3.SetIfConnected(ha.HLC3)
+	blk.occ3.SetIfConnected(ha.OCC3)
+	blk.ohlc4.SetIfConnected(ha.OHLC4)
+	blk.hlcc4.SetIfConnected(ha.HLCC4)
+}
+
+func (blk *HeikinAshi) updatePrev(cur *models.Bar) {
+	blk.prev = cur.OHLC
 }
