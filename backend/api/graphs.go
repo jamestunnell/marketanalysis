ae76d93a821e74b35115e87719be9523937f64ef
James Tunnell <jamestunnell@gmail.com>
2024-04-29 17:34:45 +0000

    Validate param values

--- backend/api/graphs.go
+++ backend/api/graphs.go
@@ -5,8 +5,10 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/jamestunnell/marketanalysis/backend/models"
 	"github.com/jamestunnell/marketanalysis/blocks/registry"
+	"github.com/xeipuuv/gojsonschema"
 )
 
 func MakeGraphsAPI(db *mongo.Database) (*API[models.GraphDef], error) {
@@ -30,8 +32,34 @@ func MakeGraphsAPI(db *mongo.Database) (*API[models.GraphDef], error) {
 
 				names[b.Name] = 1
 
-				if _, found := registry.Get(b.Type); !found {
+				newBlk, blkFound := registry.Get(b.Type)
+				if !blkFound {
 					return fmt.Errorf("unknown block type '%s'", b.Type)
+				}
+
+				params := newBlk().GetParams()
+
+				for name, val := range b.ParamVals {
+					param, found := params[name]
+					if !found {
+						return fmt.Errorf("unknown param name '%s'", name)
+					}
+
+					l := gojsonschema.NewGoLoader(param.GetSchema())
+
+					schema, err := gojsonschema.NewSchema(l)
+					if err != nil {
+						return fmt.Errorf("failed to compile schema for param %s: %w", name, err)
+					}
+
+					result, err := schema.Validate(gojsonschema.NewGoLoader(val))
+					if err != nil {
+						return fmt.Errorf("failed to validate value %v for param %s: %w", val, name, err)
+					}
+
+					if !result.Valid() {
+						return newValidateParamValErr(name, val, result)
+					}
 				}
 			}
 
@@ -40,4 +68,18 @@ func MakeGraphsAPI(db *mongo.Database) (*API[models.GraphDef], error) {
 	}
 
 	return a, nil
+}
+
+func newValidateParamValErr(
+	name string,
+	val any,
+	result *gojsonschema.Result,
+) error {
+	var merr *multierror.Error
+
+	for _, resultErr := range result.Errors() {
+		merr = multierror.Append(merr, fmt.Errorf("%s", resultErr.String()))
+	}
+
+	return fmt.Errorf("param %s value %v is invalid: %w", name, val, merr)
 }
