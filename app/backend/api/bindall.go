cb09ecdc607c7bdd419ddccbbed5d408aa6a7b27
James Tunnell <jamestunnell@gmail.com>
2024-05-03 18:48:58 +0000

    Extract a common stores package

--- app/backend/api/bindall.go
+++ app/backend/api/bindall.go
@@ -4,20 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/rs/zerolog/log"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func BindAll(r *mux.Router, db *mongo.Database) {
-	securities, err := NewSecurities(db)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to make securities API")
-	}
-
-	graphs, err := NewGraphs(db, securities)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to make securities API")
-	}
+	securities := NewSecurities(db)
+	graphs := NewGraphs(db, securities)
 
 	r.Handle("/blocks/{type}", NewGetBlockInfo()).Methods(http.MethodGet)
 	r.Handle("/blocks", NewGetAllBlockInfo()).Methods(http.MethodGet)
